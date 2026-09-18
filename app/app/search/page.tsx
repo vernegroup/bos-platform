1202f3a226693520780c0460ebd24b6ea14923ad
vernegroup <vernegroup@op.pl>
2026-09-18 14:10:26 +0200

    Add BOS application routes

---
+++ app/app/search/page.tsx
@@ -0,0 +1,2 @@
+import AppPlaceholder from "@/components/app-shell/AppPlaceholder";
+export default function SearchPage(){return <AppPlaceholder kicker="BOS / WYSZUKIWARKA" title="Wyszukiwarka" description="Docelowe globalne wyszukiwanie informacji zapisanych przez organizację w modułach BOS." scope={["Standardy i czynności","Wdrożenia i procesy","Użytkownicy","Pliki i historia"]}/>;}
