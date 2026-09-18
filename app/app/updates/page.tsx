593db8c2dee00afd918f17ed7ad82ab842aaafef
vernegroup <vernegroup@op.pl>
2026-09-18 14:10:34 +0200

    Add BOS application routes

---
+++ app/app/updates/page.tsx
@@ -0,0 +1,2 @@
+import AppPlaceholder from "@/components/app-shell/AppPlaceholder";
+export default function UpdatesPage(){return <AppPlaceholder kicker="BOS / AKTUALIZACJE" title="Aktualizacje" description="Historia zmian dostarczanych centralnie do zakupionych produktów BOS." scope={["Nowe wersje produktów","Opis zmian","Data publikacji","Historia aktualizacji"]}/>;}
