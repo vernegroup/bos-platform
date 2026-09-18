aae085b45136f7d541531afef531c12e204d2090
vernegroup <vernegroup@op.pl>
2026-09-18 14:10:39 +0200

    Add BOS application routes

---
+++ app/app/onboarding/page.tsx
@@ -0,0 +1,2 @@
+import AppPlaceholder from "@/components/app-shell/AppPlaceholder";
+export default function OnboardingEntryPage(){return <AppPlaceholder kicker="BOS / ONBOARDING" title="BOS Onboarding" description="Routing produktu jest gotowy. Właściwy panel Onboardingu powstanie w punkcie 5 planu." scope={["Przygotuj","Przeprowadź","Zamknij"]}/>;}
