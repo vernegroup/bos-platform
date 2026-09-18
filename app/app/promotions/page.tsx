7561ebd325c22d0e0c10b6a83b4ce80fc43075f2
vernegroup <vernegroup@op.pl>
2026-09-18 14:10:42 +0200

    Add BOS application routes

---
+++ app/app/promotions/page.tsx
@@ -0,0 +1,2 @@
+import AppPlaceholder from "@/components/app-shell/AppPlaceholder";
+export default function PromotionsEntryPage(){return <AppPlaceholder kicker="BOS / PROMOTIONS" title="BOS Promotions" description="Moduł ma własne miejsce w platformie. Jego logika produktowa zostanie wdrożona po ustabilizowaniu BOS Core i Onboardingu." scope={["Procesy awansu","Zmiany ról","Historia procesów"]}/>;}
