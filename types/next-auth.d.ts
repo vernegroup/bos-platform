51aa70e7acb51dc71fd02a4b791cf4fdaf5580ad
vernegroup <vernegroup@op.pl>
2026-09-18 15:36:54 +0200

    Fix Auth.js session user typing

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,6 +1,11 @@
 import "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    provider?: string;
+    providerAccountId?: string;
+  }
+
   interface Session {
     user: {
       id: string;
