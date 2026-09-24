a1dbc8e1d430bb0a50174212ec8813852804a059
vernegroup <vernegroup@op.pl>
2026-09-24 21:13:56 +0200

    FIX hide product and story sections from public home

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import "./styles.css";
 import "./product-stage.css";
 import "./product-flow.css";
@@ -8,16 +7,12 @@ import "./mobile.css";
 import "./product-alignment.css";
 import "./public-scene.css";
 import "./public-hero.css";
-import "./public-products.css";
-import "./public-story.css";
 
 import TopBar from "../components/TopBar";
 import BottomBar from "../components/BottomBar";
 import BOSSupport from "../components/BOSSupport/BOSSupport";
-import ProductRail from "../components/home/ProductRail";
 import PublicScene from "../components/home/PublicScene";
 import PublicHero from "../components/home/PublicHero";
-import PublicStory from "../components/home/PublicStory";
 
 export default function HomePage() {
   return (
@@ -25,15 +20,9 @@ export default function HomePage() {
       <PublicScene />
       <div className="bos-public-content">
         <TopBar />
-
         <main className="bos-home">
           <PublicHero />
-
-          <ProductRail />
-
-          <PublicStory />
         </main>
-
         <BottomBar />
         <BOSSupport />
       </div>
